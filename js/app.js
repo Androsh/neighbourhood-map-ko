b3d195c903f64fcffc646cf6e53c4713841ae904
androsh <apanasiuk00@gmail.com>
2017-04-19 07:39:11 +0100

    feat: Markers on the map added

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ var map;
 var markers = [];
 
 $(document).ready(function() {
-    $("#menu").click(function() {
-        $("#header").toggleClass('full'),
-        $(".side-box").toggleClass('hide')
+    $('#menu').click(function() {
+        $('#header').toggleClass('full'),
+        $('.side-box').toggleClass('hide')
     })
 })
 
@@ -88,4 +88,50 @@ function initMap() {
          mapTypeControl: false
     });
 
+    var locations = [
+        {title: 'Victoria and Albert Museum', location: {lat: 51.496820, lng: -0.172169}},
+        {title: 'The British Museum', location: {lat: 51.519587, lng: -0.126978}},
+        {title: 'Museum of London', location: {lat: 51.517805, lng: -0.096746}},
+        {title: 'Science Museum', location: {lat: 51.497970, lng: -0.174513}},
+        {title: 'Natural History Museum', location: {lat: 51.496895, lng: -0.176410}},
+        {title: 'National Maritime Museum', location: {lat: 51.481049, lng: -0.005257}},
+        {title: 'Tate Modern', location: {lat: 51.507742, lng: -0.099346}},
+        {title: 'Imperial War Museum', location: {lat: 51.495764, lng: -0.108318}},
+        {title: 'National Army Museum', location: {lat: 51.486907, lng: -0.160508}},
+        {title: 'V&A Museum of Childhood', location: {lat: 51.529208, lng: -0.054941}},
+        {title: 'National Gallery', location: {lat: 51.509096, lng: -0.128288}}
+    ];
+
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var i = 0; i < locations.length; i++) {
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        markers.push(marker);
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+
+}
+
+function populateInfoWindow(marker, infowindow) {
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+        });
+    }
 }
