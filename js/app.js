d63eca435e41504cc723317d7fd7941b653458a8
androsh <apanasiuk00@gmail.com>
2017-07-25 00:39:52 +0100

    feat: Search/filter functionality, bounce animation on markers

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-// 'use strict';
-
 var map;
-//new blank array for all markers
-var markers = [];
 
 $(document).ready(function() {
     $('#menu').click(function() {
@@ -21,61 +17,94 @@ function initMap() {
          mapTypeControl: false
     });
 
-    var largeInfowindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-
-    for (var i = 0; i < locations.length; i++) {
-        var position = locations[i].location;
-        var title = locations[i].title;
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-        });
-        markers.push(marker);
-        marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-        });
-
+    for(var i=0; i<locations.length; i++){
+        addMarker(locations[i]);
     }
 
 }
+//function to add marker to the map
+function addMarker(locations){
+  var latLong = {
+    lat: locations.location.lat,
+    lng: locations.location.lng
+  };
+  self.marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: latLong
+  });
 
-function populateInfoWindow(marker, infowindow) {
-    if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-        });
-    }
+  if(self.marker){
+    self.markerArray().push([latLong, self.marker]);
+    google.maps.event.addListener(marker, "click", function(){
+      stopAnimation();
+      startAnimation(latLong);
+    });
+  }
+}
+
+//function to remove all the marker from the map
+function removeMarker(){
+  for(var i=0; i<self.markerArray().length; i++){
+    self.markerArray()[i][1].setVisible(false);
+  }
+}
+
+//fuction show all the makers on the map
+function showMarker(){
+  for(var i=0; i<self.markerArray().length; i++){
+    self.markerArray()[i][1].setVisible(true);
+  }
+}
+
+//function to display bounce animation on the marker when clicked.
+function startAnimation(latLong){
+  ko.computed(function(){
+    ko.utils.arrayForEach(self.markerArray(), function(m){
+      if(latLong.lat === m[0].lat && latLong.lng === m[0].lng){
+        m[1].setVisible(true);
+        m[1].setAnimation(google.maps.Animation.BOUNCE);
+      }
+    });
+  });
+}
+
+//function to stop the animation on markers.
+function stopAnimation(){
+  for(var i=0; i<self.markerArray().length; i++){
+    self.markerArray()[i][1].setAnimation(null);
+  }
 }
 
 function viewModel() {
 
     var self = this;
+    this.markerArray = ko.observableArray([]);
     this.query = ko.observable();
     this.title = ko.observable(locations.title);
 
     this.searchResults = ko.computed(function() {
         i = self.query();
         if(!i){
+            showMarker();
             return locations;
         }
         else{
+            removeMarker();
             return ko.utils.arrayFilter(locations, function(location) {
                 if(location.title.toLowerCase().indexOf(i) >= 0) {
+                    addMarker(location);
                     return location;
                 }
             });
         }
     })
+
+    this.viewLocations = function(locations){
+        var latLong = {lat: locations.location.lat, lng: locations.location.lng};
+        stopAnimation();
+        startAnimation(latLong);
+    };
 }
 
 ko.applyBindings(viewModel);
-
-
-
