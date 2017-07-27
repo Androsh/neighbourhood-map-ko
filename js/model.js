ceb8fe1e22c459ded898d47ed894d1fdd3879564
androsh <apanasiuk00@gmail.com>
2017-07-27 03:04:11 +0100

    refactor: Changes in model

--- js/model.js
+++ js/model.js
@@ -2,12 +2,10 @@ var locations = [
         {title: 'Victoria and Albert Museum', venue_id: '4ae2d9f8f964a5208d8f21e3', location: {lat: 51.496820, lng: -0.172169}},
         {title: 'The British Museum', venue_id: '4ac518d2f964a5203da720e3', location: {lat: 51.519587, lng: -0.126978}},
         {title: 'Museum of London', venue_id: '4ac518d2f964a5203ca720e3', location: {lat: 51.517805, lng: -0.096746}},
-        {title: 'Science Museum', venue_id: '4ac518d2f964a5203ca720e3', location: {lat: 51.497970, lng: -0.174513}},
         {title: 'Natural History Museum', venue_id: '4ac518cdf964a520e8a520e3', location: {lat: 51.496895, lng: -0.176410}},
         {title: 'National Maritime Museum', venue_id: '4ac518d2f964a52040a720e3', location: {lat: 51.481049, lng: -0.005257}},
         {title: 'Tate Modern', venue_id: '4ac518d3f964a52076a720e3', location: {lat: 51.507742, lng: -0.099346}},
         {title: 'Imperial War Museum', venue_id: '4ac518d3f964a52078a720e3', location: {lat: 51.495764, lng: -0.108318}},
         {title: 'National Army Museum', venue_id: '58e76a76ce66aa343422c1c6', location: {lat: 51.486907, lng: -0.160508}},
         {title: 'V&A Museum of Childhood', venue_id: '4c557d1506901b8d2d1a8f4d', location: {lat: 51.529208, lng: -0.054941}},
-        {title: 'National Gallery', venue_id: '4c557d1506901b8d2d1a8f4d', location: {lat: 51.509096, lng: -0.128288}}
 ];
